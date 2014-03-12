da95f71a1bda347528f0a18efa053df894065640
Jozef Izso <jozef.izso@gmail.com>
2014-03-13 00:52:11 +0100

    Copy also HTML files to output directory.

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -249,6 +249,7 @@ module.exports = function (grunt) {
           src: [
             '*.{ico,png,txt}',
             '.htaccess',
+            '{,*/}*.html',
             'img/{,*/}*.{webp}',
             'css/fonts/{,*/}*.*'
           ]
